c32d7fd5cb8436d188b6aab3e7d5c92e7b09019e
Artur4566 <artur.kesh2007@gmail.com>
2026-03-18 12:57:39 +0000

    Добавлены комментарии на русском языке

--- greeting.cpp
+++ greeting.cpp
@@ -3,9 +3,14 @@
 
 int main()
 {
+    // Переменная для хранения имени пользователя
     std::string username;
+    
+    // Запрос имени у пользователя
     std::cout << "Введите ваше имя: ";
     std::cin >> username;
+    
+    // Вывод приветствия
     std::cout << "Привет, " << username << "!" << std::endl;
     return 0;
 }
